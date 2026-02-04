9bbea1c879451d4b498799323b906d60448927a3
MinsooKang <214691@jnu.ac.kr>
2026-02-04 20:43:15 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 80 ms, Memory: 2284 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, K;
+    cin >> N >> K;
+    list<int> L;
+    for (int i = 1; i <= N; i++){
+        L.push_back(i);
+    }
+    auto cursor = L.begin();
+    cout << "<";
+    while (!L.empty()){
+        for (int i = 0; i < K-1; i++){
+            cursor++;
+            if (cursor == L.end()){
+                cursor = L.begin();
+            }
+        }
+        cout << *cursor;
+
+        cursor = L.erase(cursor);
+
+        if (cursor == L.end()){
+            cursor = L.begin();
+        }
+
+        if (!L.empty()){
+            cout << ", ";
+        }
+    }
+    cout << ">";
+}
