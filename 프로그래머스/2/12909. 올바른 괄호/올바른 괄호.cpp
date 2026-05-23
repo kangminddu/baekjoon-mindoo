50d08b45db49bba5e042cf250e9cb5deb024b6d3
MinsooKang <214691@jnu.ac.kr>
2026-05-23 15:49:07 +0900

    [level 2] Title: 올바른 괄호, Time: 0.35 ms, Memory: 3.86 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s){
+    stack<char> st;
+    
+    for (char c : s){
+        if (c == '('){
+            st.push(c);
+        }
+        else{
+            if (st.empty()){
+                return false;
+            }
+            st.pop();
+        }
+    }
+    return st.empty();
+}
