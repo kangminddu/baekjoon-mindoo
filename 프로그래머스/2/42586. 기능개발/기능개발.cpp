d61ce05bcf96f1cbe29f013b72724ce50a618bd2
MinsooKang <214691@jnu.ac.kr>
2026-05-23 15:25:13 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.25 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -8,14 +8,13 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> days;
     
     for (int i = 0; i < progresses.size(); i++){
-        int remain = 100 - progresses[i];
-        if (remain % speeds[i] == 0) days.push_back(remain / speeds[i]);
-        else days.push_back(remain / speeds[i] + 1);
+        if ((100 - progresses[i]) % speeds[i] == 0) days.push_back((100 - progresses[i]) / speeds[i]);
+        else days.push_back((100 - progresses[i]) / speeds[i] + 1);
     }
     int current = days[0];
     int count = 1;
     for (int i = 1; i < days.size(); i++){
-        if (days[i] <= current) count++;
+        if (current >= days[i]) count++;
         else{
             answer.push_back(count);
             current = days[i];
