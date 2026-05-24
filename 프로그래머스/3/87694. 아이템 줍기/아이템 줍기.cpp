b4713aafdf170bd38414ea6cdcd28794117e88ac
MinsooKang <214691@jnu.ac.kr>
2026-05-24 17:32:34 +0900

    [level 3] Title: 아이템 줍기, Time: 0.03 ms, Memory: 3.93 MB -BaekjoonHub

---
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
@@ -0,0 +1,97 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int board[102][102];
+bool visited[102][102];
+
+int dx[4] = {-1,0,1,0};
+int dy[4] = {0,1,0,-1};
+
+int solution(vector<vector<int>> rectangle,
+             int characterX,
+             int characterY,
+             int itemX,
+             int itemY) {
+
+    // 1. 직사각형 전체 채우기
+    for(auto r : rectangle){
+
+        int x1 = r[0] * 2;
+        int y1 = r[1] * 2;
+        int x2 = r[2] * 2;
+        int y2 = r[3] * 2;
+
+        for(int x = x1; x <= x2; x++){
+            for(int y = y1; y <= y2; y++){
+
+                board[x][y] = 1;
+            }
+        }
+    }
+
+    // 2. 내부 제거
+    for(auto r : rectangle){
+
+        int x1 = r[0] * 2;
+        int y1 = r[1] * 2;
+        int x2 = r[2] * 2;
+        int y2 = r[3] * 2;
+
+        for(int x = x1 + 1; x < x2; x++){
+            for(int y = y1 + 1; y < y2; y++){
+
+                board[x][y] = 0;
+            }
+        }
+    }
+
+    // BFS
+    queue<pair<pair<int,int>, int>> q;
+
+    q.push({{characterX * 2, characterY * 2}, 0});
+
+    visited[characterX * 2][characterY * 2] = true;
+
+    while(!q.empty()){
+
+        int x = q.front().first.first;
+        int y = q.front().first.second;
+        int dist = q.front().second;
+
+        q.pop();
+
+        // 아이템 도착
+        if(x == itemX * 2 && y == itemY * 2){
+
+            return dist / 2;
+        }
+
+        // 4방향 탐색
+        for(int i = 0; i < 4; i++){
+
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            // 범위 밖
+            if(nx < 0 || ny < 0 || nx >= 102 || ny >= 102)
+                continue;
+
+            // 이미 방문
+            if(visited[nx][ny])
+                continue;
+
+            // 이동 불가
+            if(board[nx][ny] == 0)
+                continue;
+
+            visited[nx][ny] = true;
+
+            q.push({{nx, ny}, dist + 1});
+        }
+    }
+
+    return 0;
+}
