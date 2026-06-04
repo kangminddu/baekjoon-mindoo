73b775d726355fb1266e45f31d56cdf5679ee553
MinsooKang <214691@jnu.ac.kr>
2026-06-04 20:46:57 +0900

    [level 3] Title: 네트워크, Time: 0.04 ms, Memory: 3.95 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 using namespace std;
+
 void dfs(int now, int n, vector<vector<int>>& computers, vector<bool>& visited){
     visited[now] = true;
     for (int next = 0; next < n; next++){
@@ -12,12 +13,12 @@ void dfs(int now, int n, vector<vector<int>>& computers, vector<bool>& visited){
 }
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    vector<bool> visited(n,false);
+    vector<bool> visited(n, false);
     for (int i = 0; i < n; i++){
         if (!visited[i]){
-            dfs(i,n,computers,visited);
+            dfs(i, n, computers, visited);
             answer++;
-        }        
+        }
     }
     return answer;
 }
