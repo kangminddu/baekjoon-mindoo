d41a08b1c14490283ea549749225bc8fbff29c73
MinsooKang <214691@jnu.ac.kr>
2026-09-23 15:56:08 +0900

    [level 2] Title: 타겟 넘버, Time: 2.33 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,13 +1,14 @@
+#include <string>
 #include <vector>
+
 using namespace std;
 
-int dfs(const vector<int>& numbers, int target, int index, int sum) {
-    if (index == numbers.size()) {
-        return sum == target ? 1 : 0;
+int dfs(vector<int>& numbers, int target, int index, int count){
+    if (index == numbers.size()){
+        return count == target ? 1 : 0;
     }
-
-    return dfs(numbers, target, index + 1, sum + numbers[index])
-         + dfs(numbers, target, index + 1, sum - numbers[index]);
+    return dfs(numbers, target, index + 1, count + numbers[index]) +
+        dfs(numbers, target, index + 1, count - numbers[index]);
 }
 
 int solution(vector<int> numbers, int target) {
